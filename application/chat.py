d83e8b4a479b965e23940fee4c549fa46f46f789
John Park <kyopark2014@gmail.com>
2025-01-06 21:03:53 +0900

    update

--- application/chat.py
+++ application/chat.py
@@ -578,9 +578,9 @@ def get_references(docs):
         print('excerpt(quotation removed): ', excerpt)
         
         if page:                
-            reference = reference + f"{i+1}. {page}page in [{name}]({url})), {excerpt[:50]}...\n"
+            reference = reference + f"{i+1}. {page}page in [{name}]({url})), {excerpt[:30]}...\n"
         else:
-            reference = reference + f"{i+1}. [{name}]({url}), {excerpt[:50]}...\n"
+            reference = reference + f"{i+1}. [{name}]({url}), {excerpt[:30]}...\n"
     return reference
 
 
